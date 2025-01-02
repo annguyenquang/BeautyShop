7a2bd3fac7e3f78963ae4aa7294835dc52df32e9
Phuoc Phan <21520405@gm.uit.edu.vn>
2025-01-02 23:36:44 +0700

    done chatbox ai assistant

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CartCountProvider } from "./context/CartContext.jsx";
-import { WishCountProvider } from './context/WishlistContext.jsx';
+import { WishCountProvider } from "./context/WishlistContext.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import Aos from "./components/Aos.jsx";
+import ChatBox from "./components/Home/ChatBox/ChatBox.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <BrowserRouter onChange={() => window.scrollTo(0, 0)}>
             <Aos>
               <App />
+              <ChatBox />
               <ScrollToTop />
             </Aos>
           </BrowserRouter>
